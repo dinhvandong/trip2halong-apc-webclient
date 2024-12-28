c6e7a099570fbd5fd60dd22dc1cf7c80343f350f
anhtoan-VTI <bilong2468@gmail.com>
2024-12-28 20:48:26 +0700

    toan
    toan

--- trip2halongclient/src/App.js
+++ trip2halongclient/src/App.js
@@ -1,7 +1,11 @@
+import Toan from './components/toan/Toan';
+import Toan2 from './components/toan/Toan2';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-col w-full h-screen p-4 bg-gray-200">
+      <Toan/>
+      <Toan2/>
+    </div>
+  );
 }
