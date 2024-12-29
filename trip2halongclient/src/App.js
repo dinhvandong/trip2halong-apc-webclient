af46bcde53a5a7f7c44c2c55023f4274bc004fe2
anhtoan-VTI <bilong2468@gmail.com>
2024-12-30 00:28:27 +0700

    toanbo
    toanbo

--- trip2halongclient/src/App.js
+++ trip2halongclient/src/App.js
@@ -2,16 +2,13 @@ import React from 'react'
 import Kien from './components/kien/Kien'
 import Huy from './components/huy/Huy'
 import Quan from './components/quan/Quan'
+import Toanbo from './components/Toanbobo/Toanbo'
 
 const App = () => {
   return (
     <div className='flex flex-col '>
 
-      <Kien/>
-      <Huy/>
-      <Quan/>
-      <Quan/>
-
+      <Toanbo/>
 
 
     </div>
