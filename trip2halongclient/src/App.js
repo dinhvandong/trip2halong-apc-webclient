4e2fb48aeeb1f90c040869e45318c30db6bdc039
Chickien <kienhchi@gmail.com>
2024-12-29 22:08:31 +0700

    header-kien

--- trip2halongclient/src/App.js
+++ trip2halongclient/src/App.js
@@ -1,7 +1,8 @@
+import Kien from "./components/kien/Kien"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-auto w-auto">
+        <Kien/>
+    </div>
   )
 }
