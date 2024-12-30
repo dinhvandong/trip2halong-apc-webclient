0444b9c9d1d15655c54e2c0b5755c8b1ca9397f7
dinhvandong <dongvan.ict3@gmail.com>
2024-12-30 17:25:06 +0700

    update code

--- trip2halongclient/src/pages/ResetPage.jsx
+++ trip2halongclient/src/pages/ResetPage.jsx
@@ -3,13 +3,14 @@ import Reset from '../components/kien/Reset'
 import Kien from '../components/kien/Kien'
 import VuLong from '../components/vulong/VuLong'
 import Header from '../components/kien/Header'
+import Footer from '../components/vulong/Footer'
 
 const ResetPage = () => {
   return (
     <div className='w-full flex flex-col'>
     <Header/>
     <Reset/>
-    <VuLong/>
+    <Footer/>
 
 </div>  )
 }
--- trip2halongclient/src/pages/SignUpPage.jsx
+++ trip2halongclient/src/pages/SignUpPage.jsx
@@ -3,13 +3,14 @@ import Kien from '../components/kien/Kien'
 import VuLong from '../components/vulong/VuLong'
 import Duc from '../components/duc/Duc'
 import Header from '../components/kien/Header'
+import Footer from '../components/vulong/Footer'
 
 const SignUpPage = () => {
   return (
     <div className='w-full flex flex-col'>
         <Header/>
         <Duc/>
-        <VuLong/>
+        <Footer/>
 
     </div>
   )
